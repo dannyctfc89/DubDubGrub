146d2616b75da3e75597e1868a43b1f53ee16514
Danny Cummings <dannycbg89@gmail.com>
2021-06-26 22:52:00 +0100

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Daniel Cummings on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
